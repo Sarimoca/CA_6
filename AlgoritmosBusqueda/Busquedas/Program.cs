8e9b78e438c25a1b71e56d8dd1b7b305db9a775e
Samuel Morales Cascante <sarimoca@gmail.com>
2025-05-03 19:21:12 -0600

    Implementación y pruebas unitarias

---
+++ AlgoritmosBusqueda/Busquedas/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using BusquedaAlgoritmos;
+
+namespace BusquedaAlgoritmos
+{
+    public class Busquedas
+    {
+        // Búsqueda lineal (funciona con arreglos ordenados o no ordenados)
+        public static int BusquedaLineal(int[] arr, int target)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == target)
+                {
+                    return i;
+                }
+            }
+            return -1; // Retorna -1 si no se encuentra el elemento
+        }
+
+        // Búsqueda binaria (requiere que el arreglo esté ordenado)
+        public static int BusquedaBinaria(int[] arr, int target)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] == target)
+                {
+                    return mid;
+                }
+                else if (arr[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1; // Retorna -1 si no se encuentra el elemento
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
---
+++ AlgoritmosBusqueda/BusquedasTests/Test1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusquedaAlgoritmos;
+
+namespace BusquedaAlgoritmos.Tests
+{
+    [TestClass]
+    public class BusquedasTests
+    {
+        private readonly int[] _arregloDesordenado = { 5, 3, 8, 6, 2, 7, 1, 4, 9 };
+        private readonly int[] _arregloOrdenado = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        [TestMethod]
+        public void Lineal_ElementoExistente_DevuelveIndiceCorrecto()
+        {
+            int resultado = Busquedas.BusquedaLineal(_arregloDesordenado, 7);
+            Assert.AreEqual(5, resultado);
+        }
+
+        [TestMethod]
+        public void Lineal_ElementoNoExistente_DevuelveMenosUno()
+        {
+            int resultado = Busquedas.BusquedaLineal(_arregloDesordenado, 10);
+            Assert.AreEqual(-1, resultado);
+        }
+
+        [TestMethod]
+        public void Binaria_ElementoExistente_DevuelveIndiceCorrecto()
+        {
+            int resultado = Busquedas.BusquedaBinaria(_arregloOrdenado, 7);
+            Assert.AreEqual(6, resultado);
+        }
+
+        [TestMethod]
+        public void Binaria_ElementoNoExistente_DevuelveMenosUno()
+        {
+            int resultado = Busquedas.BusquedaBinaria(_arregloOrdenado, 10);
+            Assert.AreEqual(-1, resultado);
+        }
+    }
+}
